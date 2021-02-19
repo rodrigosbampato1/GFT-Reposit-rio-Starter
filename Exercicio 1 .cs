fc49e43dd24e9941573c2ae5d205d0fefc41fa27
Rodrigo Sbampato <rodrigo.sbampato@gmail.com>
2021-02-19 16:00:04 -0300

    Integração

---
+++ Exercicio 1 .cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double multiplicando, multiplicador, produto = 0;
+            for Double (multiplicandor= 1 <=3000);
+         
+
+            Console.WriteLine(" Multiplicando"); {
+
+
+                if multiplicando %> 3000;
+                    Console.Write(" Sem calculo");
+
+
+                Console.WriteLine("Inicio Intervalo ");
+                if multiplicador > 10;
+                    Console.Write("Invalido");
+
+                Console.WriteLine("Fim Intervalo");
+                if produto == multiplicador;
+                    Console.Write("O Fim do Intervalo deve não pode ser igual ao Inicio Intervalo");
+
+                Console.WriteLine(multiplicando * multiplicador);
+
+                private static multiplicando (double multiplicando double multiplicador);
+
+                return (multiplicador * multiplicando);
+
+                Console.ReadKey();
+
+
+            }
+            
+            
+
+
+            
+
+        }
+    }
+}
---
+++ Exercicio 2 .cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quadrado = 0;
+            double retangulo =  ,altura, largura;
+            int circulo = 0;
+
+
+            Console.WriteLine("Digite um numero para o quadrado: ");
+            {
+                int quadrado = ( int quadrado * int quadrado);
+                
+
+                Console.Write(" O tamanho do quadrado é " = +quadrado);
+
+            }
+
+            {
+
+                Console.WriteLine(" Digite a Altura do Retangulo: ");
+                    return retangulo(altura);
+                
+                Console.WriteLine(" Digite a Largura do Retangulo: ");
+                return double retangulo (largura * altura);
+
+                Console.Write(+ retangulo);
+
+                
+
+
+
+
+            }
+
+            {
+                Console.WriteLine(" Digite a Area do Circulo");
+                raio = CDbl(Console.ReadLine())
+                area = Math.PI * Math.Pow(raio, 2)
+                Console.WriteLine("A area do círculo é " & _raio & " é igual a " & area);
+
+                Console.ReadKey() ; 
+
+
+
+            }
+        }
+    }
+}
